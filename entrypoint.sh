1413a09303e2fe7d5c66e9795b68b49cc79a31b1
Grégory Bellencontre <gregory.bellencontre@payfit.com>
2021-02-27 17:59:05 +0100

    chore: initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if needed env vars are available
+if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $DESTINATION_PATH || -z $OVH_HOSTING_DOMAIN || -z $REPOSITORY_NAME || -z $REPOSITORY_URL ]]; then
+  echo 'One or more needed env vars are undefined! Take a look at the example usage declaration.'
+  exit 1
+fi
+
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rv * && git clone $REPOSITORY_URL && mv $REPOSITORY_NAME/* $DESTINATION_PATH && mv $REPOSITORY_NAME/.htaccess $DESTINATION_PATH && rm -rf $REPOSITORY_NAME && rm *.md LICENSE"
+echo 'Done.'
